716a2d22323520ac9bdbe42697772f09f9e28c4c
Samuel-Muriuki <sammkimberly@gmail.com>
2022-06-17 18:16:36 +0300

    Creating the Task Component and Styling it

--- src/components/AddTask.js
+++ src/components/AddTask.js
@@ -2,6 +2,7 @@ const [state, setState] = useState(initialState);
 import { useState } from 'react';
 import Swal from "sweetalert2";
 
+
 const AddTask = ({ onSave }) => {
     const [text, setText] = useState('');
     const [day, setDay] = useState('');
